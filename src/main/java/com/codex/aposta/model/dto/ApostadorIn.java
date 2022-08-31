b3ac9ab12c8fccf15a57e541fa35b4781d620047
Mariana Bastos <maribastossrodrigues@gmail.com>
2022-08-30 23:03:07 -0300

    add ApostadorControllerTest

--- src/main/java/com/codex/aposta/model/dto/ApostadorIn.java
+++ src/main/java/com/codex/aposta/model/dto/ApostadorIn.java
@@ -1,5 +1,6 @@
 package com.codex.aposta.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ApostadorIn {
     @NotBlank
     private String nome;
